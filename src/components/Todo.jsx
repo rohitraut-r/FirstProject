c19c3b88e4d242c49c84a408b53063e898f70062
rohitraut-r <rautrohit.rr123@gmail.com>
2022-09-01 10:47:42 +0545

    add input value

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Todo.css'
 
 const Todo = () => {
+  const [inputData, setinputData] = useState('');
+  const [todos, setTodos] = useState([
+    'Walk for refreshment',
+    'Walk the dog',
+    'Rest a while',
+    'Watch a movie'
+  ]);
   return (
     <div class="container">
     <h3 class = "app-logo">Todo App</h3><br></br>
-    <input type="text" class = "enter" placeholder='Enter the items'></input>
+    <input 
+        type="text" 
+        class = "enter" 
+        placeholder='Enter the items'
+        value={inputData}
+        onChange = {(e) => setinputData(e.target.value)} 
+        onClick = {handleSubmit}
+         >
+     </input>
     <button class = "add">Add Todo</button>
     <h3 class="todo-list">Todo Lists</h3>
     <ul class="list">
-        <li>Hello 1</li>
-        <li>Hello 2</li>
-        <li>Hello 3</li>
+        {todos.map((todo) =>(
+          <li>{todo}</li>
+          ))}
+         
     </ul>
     <button class="remove">Remove</button>
   </div>
